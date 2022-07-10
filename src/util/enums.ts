106c887f7332b1d48daa75dc656ce55b6c6009e0
Sayan Chaudhry <shiftsayan@users.noreply.github.com>
2022-07-10 19:39:35 -0400

    Add new files (#6)
    * Refactor
    
    * Add new ts files

---
+++ src/util/enums.ts
@@ -0,0 +1,84 @@
+export enum Phase {
+    Preflight = "PREFLIGHT",
+    Goal = "GOAL",
+    GoldenBorderDiscard = "GOLDEN_BORDER_DISCARD",
+    GoldenBorderAccept = "GOLDEN_BORDER_ACCEPT",
+    Communication = "COMMUNICATION",
+    StartTrick = "START_TRICK",
+    PlayTrick = "PLAY_TRICK",
+    Result = "RESULT",
+}
+
+export enum Suite {
+    Black,
+    Blue,
+    Red,
+    Yellow,
+    Green,
+}
+
+export enum Decoration {
+    None,
+    Blank,
+}
+
+export enum Communication {
+    None,
+    NotCommunicated,
+    DeadSpot,
+    Communicating,
+    Lowest,
+    Only,
+    Highest,
+}
+
+export enum Order {
+    None,
+    One,
+    Two,
+    Three,
+    First,
+    Second,
+    Third,
+    Last,
+}
+
+export enum Agent {
+    None,
+    All,
+    Current,
+    Next,
+    Commander,
+    Winner,
+}
+
+export enum Move {
+    None,
+    StartGame,
+    ToggleGoal,
+    SkipGoldenBorder,
+    CommunicateCard,
+    CommunicateValue,
+    PlayCard,
+    StartTrick,
+}
+
+export enum View {
+    None,
+    Table,
+    Trick,
+}
+
+export enum GoldenBorder {
+    None,
+    Available,
+    Used,
+    Skipped,
+}
+
+export enum Condition {
+    None,
+    InProgress = "IN_PROGRESS",
+    Lost = "LOST",
+    Won = "WON",
+}
---
+++ src/util/missions.ts
@@ -0,0 +1,8 @@
+import { planet_x } from "./planet_x"
+import { deep_sea } from "./deep_sea"
+
+export const missions = {
+	'planet_x': planet_x,
+	'deep_sea': deep_sea,
+}
+
