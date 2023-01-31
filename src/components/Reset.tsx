1980d5720da5815d65272b20dbd46f5a09c8bc21
shiftsayan <sayan.chaudhry@gmail.com>
2023-01-30 20:06:37 -0500

    Add enter ability

--- src/components/Reset.tsx
+++ src/components/Reset.tsx
@@ -5,30 +5,30 @@ import { ref, update } from "@firebase/database";
 import { database } from "../services/firebase";
 
 export function Reset() {
-  const [crewname] = useState("thethecrewcrew");
+  const [crewName] = useState("thethecrewcrew");
 
   useEffect(() => {
-    if (crewname) {
+    if (crewName) {
       update(ref(database), {
-        [`crews/${crewname}/phase`]: "Preflight",
-        [`crews/${crewname}/players`]: {},
-        [`crews/${crewname}/goals`]: [],
-        [`crews/${crewname}/played_cards`]: [],
-        [`crews/${crewname}/tricks`]: [],
-        [`crews/${crewname}/leading_trick`]: [],
-        [`crews/${crewname}/leading_suite`]: null,
-        [`crews/${crewname}/leading_winner`]: null,
-        [`crews/${crewname}/commander`]: null,
+        [`crews/${crewName}/phase`]: "Preflight",
+        [`crews/${crewName}/players`]: {},
+        [`crews/${crewName}/goals`]: [],
+        [`crews/${crewName}/played_cards`]: [],
+        [`crews/${crewName}/tricks`]: [],
+        [`crews/${crewName}/leading_trick`]: [],
+        [`crews/${crewName}/leading_suite`]: null,
+        [`crews/${crewName}/leading_winner`]: null,
+        [`crews/${crewName}/commander`]: null,
       });
     }
-  }, [crewname]);
+  }, [crewName]);
 
   return (
     <>
-      {crewname ? (
-        <>Resetting `{crewname}`...</>
+      {crewName ? (
+        <>Resetting `{crewName}`...</>
       ) : (
-        <>Set `crewname` state variable using React Developer Tools</>
+        <>Set `crewName` state variable using React Developer Tools</>
       )}
     </>
   );
