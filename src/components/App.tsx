d17a75fb8895bb9c379076993619454ead89fd6e
shiftsayan <sayan.chaudhry@gmail.com>
2023-01-30 19:55:28 -0500

    remove legacy reseting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,68 +1,53 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { ref, update } from '@firebase/database';
+import { ref, update } from "@firebase/database";
 
-import { Home } from './Home';
-import { Board } from './Board';
-import { Layout } from './Layout';
+import { Home } from "./Home";
+import { Board } from "./Board";
+import { Layout } from "./Layout";
 
-import { palette } from '../util/theme/palette';
-import { ViewName } from '../util/enums';
-import { database } from '../services/firebase';
-
-let FIX = false;
-// FIX = true;
+import { palette } from "../util/theme/palette";
+import { ViewName } from "../util/enums";
+import { database } from "../services/firebase";
 
 export default function App() {
-	const params = window.location.search.split('?spoof=');
-	const spoof = params.length > 1 ? params[1] : (FIX ? "shift" : "");
+  const [state, setState] = useState({
+    player: "",
+    crew: "",
+    view: ViewName.Table,
+    palette,
+    show_toast: false,
+  });
 
-	const [state, setState] = useState({
-		// crew: "",
-		player: spoof,
-		crew: 'thethecrewcrew',
-		view: ViewName.Table,
-		palette,
-		show_toast: false,
-	});
+  const unloadAlert = (event: any) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
 
-	const unloadAlert = (event: any) => {
-		event.preventDefault();
-		event.returnValue = '';
-	};
+  const unload = () => {
+    if (state.crew && state.player) {
+      update(ref(database), {
+        [`crews/${state.crew}/active/${state.player}`]: false,
+      });
+    }
+  };
 
-	const unload = () => {
-		// if (!FIX) {
-		// 	update(ref(database), {
-		// 		[`crews/${state.crew}/active/shift`]: false,
-		// 		// TODO
-		// 		[`crews/${state.crew}/phase`]: 'Preflight',
-		// 		[`crews/${state.crew}/players`]: {},
-		// 		[`crews/${state.crew}/goals`]: [],
-		// 		[`crews/${state.crew}/played_cards`]: [],
-		// 		[`crews/${state.crew}/tricks`]: [],
-		// 		[`crews/${state.crew}/leading_trick`]: [],
-		// 		[`crews/${state.crew}/leading_suite`]: null,
-		// 		[`crews/${state.crew}/leading_winner`]: null,
-		// 		[`crews/${state.crew}/commander`]: null,
-		// 	});
-		// }
-	};
+  useEffect(() => {
+    window.addEventListener("beforeunload", unloadAlert);
+    window.addEventListener("unload", unload);
+    return () => {
+      window.removeEventListener("beforeunload", unloadAlert);
+      window.removeEventListener("unload", unload);
+    };
+  });
 
-	useEffect(() => {
-		window.addEventListener('beforeunload', unloadAlert);
-		window.addEventListener('unload', unload);
-		return () => {
-			window.removeEventListener('beforeunload', unloadAlert);
-			window.removeEventListener('unload', unload);
-		};
-	});
-
-	return (
-		<Layout state={state} setState={setState}>
-			{state.crew
-				? <Board state={state} setState={setState} />
-				: <Home state={state} setState={setState} />}
-		</Layout>
-	);
+  return (
+    <Layout state={state} setState={setState}>
+      {state.crew ? (
+        <Board state={state} setState={setState} />
+      ) : (
+        <Home state={state} setState={setState} />
+      )}
+    </Layout>
+  );
 }
