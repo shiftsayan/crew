1a44da9ad55e77cf6b17fc6e3dcef884e61421aa
Sayan Chaudhry <sayan@actively.ai>
2024-08-22 00:02:51 -0400

    levels

--- src/util/game/deepSea.ts
+++ src/util/game/deepSea.ts
@@ -22,6 +22,38 @@ const missionDeepSea10 = new MissionDeepSea(4);
 
 const missionDeepSea11 = new MissionDeepSea(8);
 
+const missionDeepSea17 = new MissionDeepSea(9);
+
+const missionDeepSea18 = new MissionDeepSea(9);
+
+const missionDeepSea19 = new MissionDeepSea(9);
+
+const missionDeepSea20 = new MissionDeepSea(10);
+
+const missionDeepSea21 = new MissionDeepSea(0);
+
+const missionDeepSea22 = new MissionDeepSea(11);
+
+const missionDeepSea23 = new MissionDeepSea(0);
+
+const missionDeepSea24 = new MissionDeepSea(12);
+
+const missionDeepSea25 = new MissionDeepSea(12);
+
+const missionDeepSea26 = new MissionDeepSea(10);
+
+const missionDeepSea27 = new MissionDeepSea(0);
+
+const missionDeepSea28 = new MissionDeepSea(14);
+
+const missionDeepSea29 = new MissionDeepSea(14);
+
+const missionDeepSea30 = new MissionDeepSea(16);
+
+const missionDeepSea31 = new MissionDeepSea(17);
+
+const missionDeepSea32 = new MissionDeepSea(18);
+
 export const deepSea = {
   1: missionDeepSea1,
   2: missionDeepSea2,
@@ -34,4 +66,20 @@ export const deepSea = {
   9: missionDeepSea9,
   10: missionDeepSea10,
   11: missionDeepSea11,
+  17: missionDeepSea17,
+  18: missionDeepSea18,
+  19: missionDeepSea19,
+  20: missionDeepSea20,
+  21: missionDeepSea21,
+  22: missionDeepSea22,
+  23: missionDeepSea23,
+  24: missionDeepSea24,
+  25: missionDeepSea25,
+  26: missionDeepSea26,
+  27: missionDeepSea27,
+  28: missionDeepSea28,
+  29: missionDeepSea29,
+  30: missionDeepSea30,
+  31: missionDeepSea31,
+  32: missionDeepSea32,
 };
